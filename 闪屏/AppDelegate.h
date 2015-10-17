e8c7cfecc747f4775d9b5064c41d56633893da68
chenhuiyi <1968281129@qq.com>
2015-10-17 16:11:16 +0800

    Initial Commit

---
+++ 闪屏/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  闪屏
+//
+//  Created by 邱读书 on 15/10/17.
+//  Copyright © 2015年 邱读书. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
